09db8bb7e4c5720c199bbfd47f032014f161da55
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2021-03-22 18:28:32 -0400

    Fix pr title regex labeler
    Fix pr title regex labeler.

--- .github/scripts/commit_title_regex_labeler.py
+++ .github/scripts/commit_title_regex_labeler.py
@@ -1,4 +1,4 @@
--*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """Labels PRs based on title. Must be run in a github action with the
 pull_request event. Adapted from scikit-learn."""
 from ghapi.all import context_github
